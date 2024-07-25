d5f1de1f6676e547a9d8cc1f9a6532fdd9a18bda
you@example.com <you@example.com>
2024-07-25 11:44:58 -0500

    ok

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        double distancia;
+        double consumoPor100Km;
+        double precioLitro;
+        double consumoTotal;
+        double costoTotal;
+
+        Scanner leerTeclado=new Scanner(System.in);
+
+        System.out.print("Digite la distancia total del viaje en Km: ");
+        distancia = leerTeclado.nextDouble();
+
+        System.out.print("Digite el consumo de combustible del automóvil (litros por cada 100 km): ");
+        consumoPor100Km = leerTeclado.nextDouble();
+
+        System.out.print("Digite el precio por litro de combustible: ");
+        precioLitro = leerTeclado.nextDouble();
+
+        consumoTotal = (distancia / 100.0) * consumoPor100Km;
+
+        costoTotal = consumoTotal * precioLitro;
+
+        System.out.println("\nResumen del viaje:");
+        System.out.println("Distancia total del viaje: " + distancia + " km");
+        System.out.println("Consumo de combustible del automóvil: " + consumoPor100Km + " litros/100 km");
+        System.out.println("Precio por litro de combustible: " + precioLitro + " moneda local");
+        System.out.printf("Consumo total de combustible necesario: %.2f litros\n", consumoTotal);
+        System.out.printf("Costo total de combustible necesario: %.2f %s\n", costoTotal, "moneda local");
+    }
+}
